3ee28d8560417e78ca844e372b505f7601857159
shashank shukla <shashank7016204503@gmail.com>
2024-08-03 00:43:44 +0530

    fix cors isssue

--- app.ts
+++ app.ts
@@ -17,7 +17,7 @@ const httpServer = createServer(app)
 const io=new Server(httpServer,{
     pingTimeout:60000,
     cors:{
-        origin:"https://j-chat-omega.vercel.app/",
+        origin:"*",
         credentials:true
     }
 })
@@ -28,7 +28,7 @@ app.use(
 
     cors(
         {
-            origin: "https://j-chat-omega.vercel.app/",
+            origin: "*",
             credentials: true
         }
 
